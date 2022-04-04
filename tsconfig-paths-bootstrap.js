a7272a18e6e03ef357c61ac1a8f857ab75380585
Spectre_ <63507141+pjp0704@users.noreply.github.com>
2022-04-04 15:20:45 +0900

    Initial commit

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,9 @@
+const tsConfig = require('./tsconfig.json');
+const tsConfigPaths = require('tsconfig-paths');
+
+tsConfigPaths.register({
+  baseUrl: tsConfig.compilerOptions.outDir,
+  paths: {
+    'src/*': ['./*'],
+  },
+});
